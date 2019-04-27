c81ab72aeaa997ed09871afac0b830cefe9dd63d
Zsolt Boda <boda0zsolt@live.com>
2019-04-27 22:10:03 +0200

    asd

--- src/app.js
+++ src/app.js
@@ -4,14 +4,9 @@ var letChangeScene = true;
 
 $(document).on("click", "a", (e) => {
 	e.preventDefault();
-	if (letChangeScene) {
-		letChangeScene = false;
-		$("#fade").fadeIn(200, () => {
-			$("#content").load(e.target.getAttribute("href") + ".html", () => {
-				$("#fade").fadeOut(200, () => {
-					letChangeScene = true;
-				});
-			});
+	$("#fade").fadeIn(200, () => {
+		$("#content").load(e.target.getAttribute("href") + ".html", () => {
+			$("#fade").fadeOut(200);
 		});
-	}
+	});
 });
