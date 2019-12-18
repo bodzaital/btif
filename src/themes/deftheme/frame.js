693788a81a84f900768d5a6e76fbe0592953ca06
Zsolt Boda <boda0zsolt@live.com>
2019-12-18 11:16:43 +0100

    Update frame.js

--- src/themes/deftheme/frame.js
+++ src/themes/deftheme/frame.js
@@ -7,13 +7,4 @@ $("#menu_save").addEventListener("click", () => {
 
 $("#form_load").addEventListener("change", (e) => {
 	File.Load(e);
-	document.addEventListener("file-loaded", (f) => {
-		if (f.detail === 200) {
-			console.log("Save file successfully loaded.");	
-		} else if (f.detail === 600) {
-			console.log("Version mismatch.");
-		} else {
-			console.log("Unknown error.")
-		}
-	});
 });
