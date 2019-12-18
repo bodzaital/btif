d95a626cd16c47919c76972cc59bd9fd98ad6c40
Zsolt Boda <boda0zsolt@live.com>
2019-12-18 11:13:53 +0100

    Handle loading in scene-1

--- src/scenes/scene-1/scene-1.js
+++ src/scenes/scene-1/scene-1.js
@@ -1,3 +1,9 @@
 import { $ } from "../../modules/utils.js";
 
 $("#name").innerText = data.Get("player");
+
+document.addEventListener("file-loaded", (e) => {
+	if (e.detail === 200) {
+		$("#name").innerText = data.Get("player");
+	}
+});
