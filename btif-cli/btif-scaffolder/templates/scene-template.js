473173a64407ce5f9e0b2e49c057e18cded45356
Zsolt Boda <boda0zsolt@live.com>
2020-11-06 14:50:36 +0100

    Remove CLI

--- btif-cli/btif-scaffolder/templates/scene-template.js
+++
@@ -1,7 +0,0 @@
-﻿import { $, $$, LoadFile, ResolveSceneByName, ResolveThemeByName, NullConditional, CreateElementByDescriptor, NullishCoalescingOp } from "../../modules/utils.js";
-
-document.addEventListener("file-loaded", (e) => {
-	if (e.detail === 200) {
-		// Write logic here...
-	}
-});
